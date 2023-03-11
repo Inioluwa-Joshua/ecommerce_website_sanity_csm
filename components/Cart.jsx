8c670071f529c749af848ee74c314c90153ad047
Joshua Inioluwa <joshuainioluwa15@gmail.com>
2023-03-11 23:54:27 +0100

    .

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -125,7 +125,7 @@ const Cart = () => {
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={() => handleCheckout()}>
-                Pay with Stripe
+                Pay with Paystack
               </button>
             </div>
           </div>
